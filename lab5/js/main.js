e8a1c8194a818d3cf85ec6360fc8e6d3737925a0
Tanzina Akter <tanzina3615@diu.edu.bd>
2022-10-21 18:06:25 -0400

    Add files via upload

--- lab5/js/main.js
+++ lab5/js/main.js
@@ -3,15 +3,17 @@ fullname = document.getElementById("name");
 address = document.getElementById("address");
 email = document.getElementById("email");
 password = document.getElementById("password");
+confPassword = document.getElementById("confPassword");
 phone = document.getElementById("phone");
 comment = document.getElementById("comment");
 
-fullname_error_1 = "Please Enter your Name";
+fullname_error_1 = "Please enter your name";
 address_error_1 = "Please enter your address";
-email_error_1 = "Please enter your email";
-password_error_1 = "Please enter a password";
-phone_error_1 = "Please enter your phone number";
+email_error_1 = "Please enter  your email";
+password_error_1 = "Password cannot be empty";
+phone_error_1 = "Please enter phone number";
 comment_error_1 = "Please write your comment";
+
 fullname_error_2 = "Your name cannot contain special character or numbers";
 address_error_2 = "Address should be at least 20 characters long";
 email_error_2 = "Please enter a valid email address";
@@ -28,28 +30,13 @@ regPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
 
 regPhone = /^\d{10}$/; //validate a phone number of 10 digits with no comma, no spaces, no punctuation and there will be no + sign in front the number. Simply the validation will remove all non-digits and permit only phone numbers with 10 digits
 
-function fieldValidation(field, Regex, Error1, Error2, event) {
-  console.log(field);
+function validateField(field, regex, error1, error2, event) {
   if (field.value == "") {
-    field.nextElementSibling.innerHTML = Error1;
+    field.nextElementSibling.innerHTML = error1;
     event.preventDefault();
     return false;
-  } else if (!field.value.match(Regex)) {
-    field.nextElementSibling.innerHTML = Error2;
-    event.preventDefault();
-    return false;
-  } else {
-    field.nextElementSibling.innerHTML = "";
-    return true;
-  }
-}
-function otherValidation(field, Error1, Error2, event) {
-  if (field.value == "") {
-    field.nextElementSibling.innerHTML = Error1;
-    event.preventDefault();
-    return false;
-  } else if (field.value.length < 20) {
-    field.nextElementSibling.innerHTML = Error2;
+  } else if (!field.value.match(regex)) {
+    field.nextElementSibling.innerHTML = error2;
     event.preventDefault();
     return false;
   } else {
@@ -58,13 +45,34 @@ function otherValidation(field, Error1, Error2, event) {
   }
 }
 
+function validateOther(field, error1, error2, event) {
+  if (field.value == "") {
+    field.nextElementSibling.innerHTML = error1;
+    event.preventDefault();
+    return false;
+  } else if (field.value.length < 20) {
+    field.nextElementSibling.innerHTML = error2;
+    event.preventDefault();
+    return false;
+  } else {
+    field.nextElementSibling.innerHTML = "";
+    return true;
+  }
+}
+function confirmPassword(password, confPassword) {
+  if (!(password.value == confPassword.value)) {
+    confPassword.nextElementSibling.innerHTML = "Passwords do not match";
+  }
+}
 function validateForm(event) {
-  fieldValidation(fullname, regName, fullname_error_1, fullname_error_2, event);
-  fieldValidation(email, regEmail, email_error_1, email_error_2, event);
-  fieldValidation(password, regPass, password_error_1, password_error_2, event);
-  fieldValidation(phone, regPhone, phone_error_1, phone_error_2, event);
-  otherValidation(address, address_error_1, address_error_2, event);
-  otherValidation(comment, comment_error_1, comment_error_2, event);
+  validateField(fullname, regName, fullname_error_1, fullname_error_2, event);
+  validateField(email, regEmail, email_error_1, email_error_2, event);
+  validateField(password, regPass, password_error_1, password_error_2, event);
+  validateField(phone, regPhone, phone_error_1, phone_error_2, event);
+  validateOther(address, address_error_1, address_error_1, event);
+  validateOther(comment, comment_error_1, comment_error_2, event);
+  confirmPassword(password, confPassword);
   event.preventDefault();
 }
+
 submitForm.addEventListener("submit", validateForm);
